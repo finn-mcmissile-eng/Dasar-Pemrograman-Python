5906d4049c3643fe39b2d1fe8c7f85084d4e8eec
Yobel Christian <finn.mcmissile259@gmail.com>
2026-01-28 13:00:01 +0700

    Create Sisa belanja.py

---
+++ Sisa belanja.py
@@ -0,0 +1,11 @@
+#Tugas Mandiri Pertemuan 2
+
+hrg = float(input("input harga telur : "))
+brt = float(input("input berat telur : "))
+total_bayar = hrg * brt
+
+tarif_angkot = 3500
+ongkos = 2 * tarif_angkot
+uang_ibu = 200000
+total_pengeluaran = ongkos + total_bayar
+sisa_uang = uang_ibu - total_pengeluaran
