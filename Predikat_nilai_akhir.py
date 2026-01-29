28b52c871fc7ac4f9e193955d43e2af7838f38af
Yobel Christian <finn.mcmissile259@gmail.com>
2026-01-29 17:29:11 +0700

    Update and rename Predikat nilai akhir.py to Predikat_nilai_akhir.py

---
+++ Predikat_nilai_akhir.py
@@ -0,0 +1,17 @@
+nama = input("Input nama mahasiswa/i: ")
+nim = input("Input NIM mahasiswa/i: ")
+nilai = int(input('Masukan Nilai Akhir : '))
+
+if Nilai >= 80:
+    print('Grade = A')
+elif Nilai >= 70:
+    print('Grade = B')
+elif Nilai >= 60:
+    print('Grade = C')
+    print("Silakan HER.")
+elif Nilai >= 40:
+    print('Grade = D')
+    print("ANDA HARUS HER!")
+else:
+    print('Grade = E')
+    print("ANDA HARUS HER!")
