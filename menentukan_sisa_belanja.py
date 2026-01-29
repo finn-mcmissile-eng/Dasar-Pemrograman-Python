b9a05ba4a057d3a47c3db572c33b2192d6f22b04
Yobel Christian <finn.mcmissile259@gmail.com>
2026-01-29 16:52:44 +0700

    Update and rename Sisa belanja.py to menentukan_sisa_belanja.py

--- menentukan_sisa_belanja.py
+++ menentukan_sisa_belanja.py
@@ -1,11 +1,11 @@
-#Tugas Mandiri Pertemuan 2
-
-hrg = float(input("input harga telur : "))
+hrg = float(input("input harga telur/kg : "))
 brt = float(input("input berat telur : "))
 total_bayar = hrg * brt
 
 tarif_angkot = 3500
 ongkos = 2 * tarif_angkot
-uang_ibu = 200000
+uang_ibu = int(input("Masukkan jumlah uang yang dibawa ibu: "))
 total_pengeluaran = ongkos + total_bayar
 sisa_uang = uang_ibu - total_pengeluaran
+
+print("Sisa uang ibu adalah ", sisa_uang)
